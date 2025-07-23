4dc0433cc8ab3eee7ff65b8100530f8d104c1921
ADPC <phamngocson171201@gmail.com>
2025-07-23 14:47:51 +0700

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   devIndicators: {
@@ -11,7 +12,14 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
